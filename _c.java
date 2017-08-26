34d8591cfc0317b5a0bf98cfc478d5b6c6570892
MarianaCampos <Fandeliadm@F1LFMCAMPOS.FANDELI.NET>
2017-08-26 14:54:52 -0500

    Mi primer commit

---
+++ _c.java
@@ -0,0 +1,10 @@
+public class _c{ //el archivo debe llamarse igual que la clase cuando esta es publica
+	private static int $;
+	public static void main(String [] main) {
+	
+	String a_b= "Algo";
+	System.out.println ($);
+	System.out.println(a_b);
+	
+	}
+}
